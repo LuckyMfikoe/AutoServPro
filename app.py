022ac3640f96a3816c82ea0d3e027eb2b01356ac
Lesego Mfikoe <mfikoelucky@gmail.com>
2025-03-04 23:51:43 +0200

    Updated login function

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from cs50 import SQL
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, SQL, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
@@ -12,28 +11,28 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-#db = SQL("sqlite:///autservpro.db")# Not Made Yet
+# Configure PostgreSQL database
+db = SQL("postgresql://username:password@localhost/autoservpro")# Not Made Yet
 
 
 @app.route('/')
-@app.route('/landing_page/')
+@app.route('/landing_page')
 def landing_page():
     return render_template('landing_page.html')
 
-@app.route('/services/')
+@app.route('/services')
 def services():
     return render_template('services.html')
 
-@app.route('/about/')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/contact/')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@app.route("/login", methods=["GET", "POST"])# Incomplete
+@app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
 
@@ -42,27 +41,27 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
+        # Ensure email was submitted
+        if not request.form.get("email"):
+            return apology("must provide email", 403)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
         # Query database for username
-        #rows = db.execute(
-        #    "SELECT * FROM users WHERE username = ?", request.form.get("username")
-        #)
+        rows = db.execute(
+            "SELECT * FROM users WHERE email = ?", request.form.get("email")
+        )
 
         # Ensure username exists and password is correct
-        #if len(rows) != 1 or not check_password_hash(
-        #    rows[0]["hash"], request.form.get("password")
-        #):
-        #    return apology("invalid username and/or password", 403)
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid email and/or password", 403)
 
         # Remember which user has logged in
-        #session["user_id"] = rows[0]["id"]
+        session["owner_id"] = rows[0]["id"]
 
         # Redirect user to home page
         return redirect("/")
@@ -88,13 +87,13 @@ def register():
     session.clear()
 
     if request.method == "POST":
-        username = request.form.get("username")
+        email = request.form.get("email")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
 
         # If any are blank
-        if not username:
-            return apology("must provide username", 400)
+        if not email:
+            return apology("must provide email", 400)
         elif not password:
             return apology("must provide password", 400)
         elif not confirmation:
@@ -109,10 +108,10 @@ def register():
 
         # Try executing query if username does not exist
         #try:
-        #    db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, gen_pass)
+        #    db.execute("INSERT INTO users (email, hash) VALUES (?, ?)", email, gen_pass)
         #    return redirect("/login")
         #except ValueError:
-        #    return apology("username already exists", 400)
+        #    return apology("email already exists", 400)
     else:
         return render_template("register.html")
 
