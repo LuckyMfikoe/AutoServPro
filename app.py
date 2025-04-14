cacdb0058b83a72ad85b0588eb3243f66973c825
Lesego Mfikoe <mfikoelucky@gmail.com>
2025-04-14 21:00:07 +0200

    Add pymysql import and configure MySQL database connection

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
+import pymysql
+
+import os
+pymysql.install_as_MySQLdb()
 
 from helpers import apology, login_required
 
